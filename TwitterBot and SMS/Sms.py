4a974e834c5bb7caf7fbc27522a78b734f3c736c
LanceParantar <75514972+LanceParantar@users.noreply.github.com>
2021-05-20 20:23:24 +0800

    Update Sms.py

--- TwitterBot and SMS/Sms.py
+++ TwitterBot and SMS/Sms.py
@@ -1,11 +1,11 @@
 from twilio.rest import Client 
  
-account_sid = 'AC9625235c06066ca5a4a3aca16daf0341' 
-auth_token = '779e6578b1bb369c2329a65bdc8fafe0' 
+account_sid = '<ACCOUNT SID' 
+auth_token = 'AUTH TOKEN' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create(  
-                              messaging_service_sid='MGd9b3d9bb0785042bb9187f296a9a23f9', 
+                              messaging_service_sid='SERVICE ID', 
                               body='VALORANT',      
                               to='+639561657728' 
                           ) 
