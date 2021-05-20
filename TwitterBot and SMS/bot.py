5e374cc023a771fbefba2109dd9916c8649eb5c4
LanceParantar <75514972+LanceParantar@users.noreply.github.com>
2021-05-20 23:52:51 +0800

    Update bot.py

--- TwitterBot and SMS/bot.py
+++ TwitterBot and SMS/bot.py
@@ -1,8 +1,8 @@
 
 import tweepy
 import time
-auth = tweepy.OAuthHandler('RZchrxd0XJVvZpwGqzT3DmiRt', 'fc2nCjfjiddrZU54F7Wz8Yp7YxjKxQ1iGvOeefKBaX8iyvCCO3')
-auth.set_access_token('747433118211022850-kGlHgDKILRd7NQRL96KPe6UuXOdEDZ1', 'HABT0UisUVak5cy4Hk8P2dhox6K0CFXEIJNy6ZPmHYbP8')
+auth = tweepy.OAuthHandler('')#TOKEN
+auth.set_access_token('') #YTOKEN
 
 api = tweepy.API(auth)
 
